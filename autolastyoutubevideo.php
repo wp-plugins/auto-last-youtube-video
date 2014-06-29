00683217218cf9bab58ad81f5f4e494060e6d8ad
davidmerinas <davidmerinas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-29 18:18:04 +0000

    First commit version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/auto-last-youtube-video/trunk@940473 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ autolastyoutubevideo.php
@@ -0,0 +1,160 @@
+<?php
+/*
+Plugin Name: Auto Last Youtube Video
+Plugin URI: http://wordpress.org/plugins/auto-last-youtube-video/
+Description: This plugin providesboth Widget and Shortcode to show latest videos from any public Youtube channel. Using [auto_last_youtube_video user='channel_name' width='450' height='320'][/auto_last_youtube_video] in a page or post will show last video uploaded to that channel and will change if another video is uploaded. The widget let you show as many videos as you want from any Youtube channel.
+Version: 1.0.0
+Author: davidmerinas
+Author URI: http://www.davidmerinas.com
+*/
+define('AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID', "widget_AUTO_LAST_YOUTUBE_VIDEO");
+$_SESSION['AUTO_LAST_YOUTUBE_VIDEO_PATH']=plugin_dir_path( __FILE__ );
+ 
+function AUTO_LAST_YOUTUBE_VIDEO_showvideo($canal='davidmerinas',$number=1,$title=""){
+	global $wp_embed;
+	$videos=youtube($canal,$number);
+	echo('<h2 class="widget-title">'.($title!=""?$title:__("Latest from Youtube","autolastyoutubevideo")).'</h2>');
+	foreach($videos as $video)
+	{
+		echo($wp_embed->run_shortcode('[embed]http://www.youtube.com/watch?v='.$video['idyoutube'].'[/embed]'));
+	}
+
+	echo('<a id="autolasvideoseeall" href="http://www.youtube.com/user/'.$canal.'" title="'.__("Visit Youtube Channel","autolastyoutubevideo").'">'.__("Visit Youtube Channel","autolastyoutubevideo").'</a>');
+}
+
+function youtube($canal,$lim=1)
+{
+	require_once 'inc/Zend/Feed.php';
+	$url="http://gdata.youtube.com/feeds/base/users/".$canal."/uploads?alt=rss&v=2&orderby=published";
+
+	$rss=new Zend_Feed();
+	$channel = $rss->import($url);
+	$i=0;
+	$respuesta=array();
+	foreach ($channel as $item)
+	{
+		if ($i<$lim)
+		{
+			$urlvideo=str_replace("&feature=youtube_gdata","",$item->link);
+			$urlvideo=str_replace("watch?","",$urlvideo);
+			$urlvideo=str_replace("=","/",$urlvideo);
+			$idyoutube=str_replace('http://www.youtube.com/v/','',$urlvideo);
+			$respuesta[]=array('idyoutube'=>$idyoutube,'imagen'=>'http://i.ytimg.com/vi/'.$idyoutube.'/maxresdefault.jpg');
+		}
+		$i++;
+	}
+
+	return $respuesta;
+}
+
+function auto_last_youtube_video_func($atts, $content = null) {
+	extract(shortcode_atts(array(
+	'user'=>'',
+	'width'=>'540',
+	'height'=>'405'
+	), $atts));
+	global $wp_embed;
+	$videos=youtube($user,1);
+	$contenido="";
+	if(count($videos)>0)
+	{
+		foreach($videos as $video)
+		{
+			$contenido='[embed width="'.$width.'" height="'.$height.'"]http://www.youtube.com/watch?v='.$video['idyoutube'].'[/embed]';
+		}
+	}
+	return $wp_embed->run_shortcode($contenido);
+}
+
+function widget_AUTO_LAST_YOUTUBE_VIDEO_control() {
+	$options = get_option(AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID);
+	if (!is_array($options)) {
+		$options = array();
+	}
+	$widget_data = $_POST[AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID];
+	if ($widget_data['submit']) {
+		$options['AUTO_LAST_YOUTUBE_VIDEO_title'] = $widget_data['AUTO_LAST_YOUTUBE_VIDEO_title'];
+		$options['AUTO_LAST_YOUTUBE_VIDEO_channelurl'] = $widget_data['AUTO_LAST_YOUTUBE_VIDEO_channelurl'];
+		$options['AUTO_LAST_YOUTUBE_VIDEO_number'] = $widget_data['AUTO_LAST_YOUTUBE_VIDEO_number'];
+		update_option(AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID, $options);
+	}
+	// Datos para el formulario
+	$AUTO_LAST_YOUTUBE_VIDEO_title = $options['AUTO_LAST_YOUTUBE_VIDEO_title'];
+	$AUTO_LAST_YOUTUBE_VIDEO_channelurl = $options['AUTO_LAST_YOUTUBE_VIDEO_channelurl'];
+	$AUTO_LAST_YOUTUBE_VIDEO_number = $options['AUTO_LAST_YOUTUBE_VIDEO_number'];
+	
+	// Codigo HTML del formulario	
+	?>
+	<p>
+	  <label for="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID;?>-title">
+	    <?php _e('Title','autolastyoutubevideo');?>
+	  </label>
+	  <input class="widefat"
+	    type="text"
+	    name="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>[AUTO_LAST_YOUTUBE_VIDEO_title]"
+	    id="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>-title"
+	    value="<?php echo $AUTO_LAST_YOUTUBE_VIDEO_title; ?>"/>
+	</p>
+	<p>
+	  <label for="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID;?>-channelurl">
+	    <?php _e('User / Channel Name','autolastyoutubevideo');?>
+	  </label>
+	  <input class="widefat"
+	    type="text"
+	    name="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>[AUTO_LAST_YOUTUBE_VIDEO_channelurl]"
+	    id="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>-channelurl"
+	    value="<?php echo $AUTO_LAST_YOUTUBE_VIDEO_channelurl; ?>"/>
+	</p>
+	<p>
+	  <label for="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID;?>-number">
+	    <?php _e('Number','autolastyoutubevideo');?>
+	  </label>
+	  <input class="widefat"
+	    type="text"
+	    name="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>[AUTO_LAST_YOUTUBE_VIDEO_number]"
+	    id="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>-number" value="<?php echo $AUTO_LAST_YOUTUBE_VIDEO_number; ?>"/>
+	</p>
+
+	<input type="hidden"
+	  name="<?php echo AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID; ?>[submit]"
+	  value="1"/>
+	<?php
+}
+
+// WIDGET
+function widget_AUTO_LAST_YOUTUBE_VIDEO($args) {
+	extract($args, EXTR_SKIP);
+	$options = get_option(AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID);
+	// Query the next scheduled post
+	$AUTO_LAST_YOUTUBE_VIDEO_title = $options["AUTO_LAST_YOUTUBE_VIDEO_title"];
+	$AUTO_LAST_YOUTUBE_VIDEO_channelurl = $options["AUTO_LAST_YOUTUBE_VIDEO_channelurl"];
+	$AUTO_LAST_YOUTUBE_VIDEO_number = $options["AUTO_LAST_YOUTUBE_VIDEO_number"];
+
+	echo $before_widget;
+	AUTO_LAST_YOUTUBE_VIDEO_showvideo($AUTO_LAST_YOUTUBE_VIDEO_channelurl,$AUTO_LAST_YOUTUBE_VIDEO_number,$AUTO_LAST_YOUTUBE_VIDEO_title);
+	echo $after_widget;
+}
+
+function widget_AUTO_LAST_YOUTUBE_VIDEO_init() {
+	wp_register_sidebar_widget(AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID,
+			__('Auto Last Youtube Video'), 'widget_AUTO_LAST_YOUTUBE_VIDEO');
+	wp_register_widget_control(AUTO_LAST_YOUTUBE_VIDEO_WIDGET_ID,
+			__('AUTO_LAST_YOUTUBE_VIDEO'), 'widget_AUTO_LAST_YOUTUBE_VIDEO_control');
+}
+
+function autolastyoutubevideo_stylesheet() {
+    // Respects SSL, Style.css is relative to the current file
+    wp_register_style( 'autolastyoutubevideo-style', plugins_url('style.css', __FILE__) );
+    wp_enqueue_style( 'autolastyoutubevideo-style' );
+}
+
+// Registrar el widget en WordPress
+load_plugin_textdomain('autolastyoutubevideo', false, basename( dirname( __FILE__ ) ) . '/languages' );
+add_action('wp_enqueue_scripts', 'autolastyoutubevideo_stylesheet');
+add_action("plugins_loaded", "widget_AUTO_LAST_YOUTUBE_VIDEO_init");
+
+//Registrar el shortcode
+add_shortcode('auto_last_youtube_video', 'auto_last_youtube_video_func');
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Auto Last Youtube Video ===
+Contributors: davidmerinas
+Donate link: http://957.be/donatedavidmerinas
+Tags: youtube,last videos,shortcode,widget,davidmerinas
+Requires at least: 3.3
+Tested up to: 3.9.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin provides both Widget and Shortcode to show latest videos from any public Youtube channel.
+
+== Description ==
+
+This plugin provides both Widget and Shortcode to show latest videos from any public Youtube channel. Using [auto_last_youtube_video user='channel_name' width='450' height='320'][/auto_last_youtube_video] in a page or post will show last video uploaded to that channel and will change if another video is uploaded. The widget let you show as many videos as you want from any Youtube channel.
+
+== Installation ==
+
+1. Uncompress the file and upload the folder ‘auto’-last-youtube-video to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+To activate the widget:
+3. Go to 'Widgets' menu and drag and drop 'Auto Last Youtube Video' widget to your prefered sidebar
+4. Configure the widget: fill in the title and channel name and select the number of video to show
+5. Click on "Save"
+
+To use the shortcode:
+6. When editing a page or a post, add the code [auto_last_youtube_video user='channel_name' width='450' height='320'][/auto_last_youtube_video] to show the last video from that channel. You can also configure width and height.
+
+
+== Frequently asked questions ==
+Not yet
+
+
+== Screenshots ==
+1. screenshot-1.png
+
+
+== Changelog ==
+
+= 1.0 =
+*First stable version
+
+== Upgrade notice ==
+Not yet
